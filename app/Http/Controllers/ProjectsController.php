a0c0a1ca598348ab2efece6dca051eab58ab10ad
Darko Vucetic <darko@kod.io>
2019-03-30 21:00:42 +0100

    inital

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return view('projects.index', compact('projects'));
+    }
+
+    public function show()
+    {
+        $project = Project::findOrFail(request('project'));
+        return view('projects.show', compact('project'));
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+        Project::create($attributes);
+        return redirect('/projects');
+    }
+
+}
